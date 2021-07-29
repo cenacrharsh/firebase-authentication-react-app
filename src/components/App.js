33d9652fb58eb9a2595bce1f38fc812a4de5c723
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-29 17:46:27 +0530

    added Login.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import { Container } from "react-bootstrap";
 /* CONTEXT */
 import { AuthProvider } from "../contexts/AuthContext";
 
+/* REAECT ROUTER */
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+/* COMPONENTS */
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+
 function App() {
   return (
     <AuthProvider>
@@ -14,6 +22,16 @@ function App() {
         style={{ minHeight: "100vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+
           <SignUp />
         </div>
       </Container>
--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,9 @@ import { useAuth } from "../contexts/AuthContext";
 /* REACT ROUTER */
 import { Link } from "react-router-dom";
 
-export default function Signup() {
+export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
 
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -21,10 +20,6 @@ export default function Signup() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match!");
-    }
 
     try {
       setError("");
@@ -41,7 +36,7 @@ export default function Signup() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
 
           {error && <Alert variant="danger">{error}</Alert>}
 
@@ -56,20 +51,15 @@ export default function Signup() {
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
 
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-
             <Button disabled={loading} className="w-100 mt-2" type="submit">
-              Sign Up
+              Log In
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="text-center w-100 mt-2">
-        Already have an account?
-        <Link to="/login">Log In</Link>
+        Need an account?
+        <Link to="/signup">Sign Up</Link>
       </div>
     </>
   );
