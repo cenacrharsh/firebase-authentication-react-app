4a68fdeee940b40d8f00c7dee135f59d4f155324
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-29 15:13:58 +0530

    completed signup page

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -13,6 +13,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
@@ -23,6 +24,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
+      setLoading(false);
     });
 
     /* onAuthStateChanged() returns a function which upon called unsubscribes the onAuthStateChanged event */
@@ -35,5 +37,11 @@ export function AuthProvider({ children }) {
     signup,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  /* WHEN WE ARE LOADING DO NOT RENDER OUT THE CHILDREN */
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
