2042674672cb10acd45f7fbf10e1cd54f8853bcd
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-29 12:01:57 +0530

    added signup form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+import SignUp from "./Signup";
+
+/* BOOTSTRAP */
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <SignUp />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+/* COMPONENTS */
 import App from "./components/App";
+
+/* BOOTSTRAP STYLES */
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
